8f228b8aa6246d761bef861a19143fb801f81b78
prasad3p <gitecprp11@gmail.com>
2016-06-27 01:14:11 -0400

    Added project source codes and relevant files/reports

---
+++ SourceCodesProjects/Prasad Patil Projects/File Transfer on UDP sockets in Java with encryption/Client java files/ClientMain.java
@@ -0,0 +1,14 @@
+package clientApplications;
+
+public class ClientMain {
+	
+	public static void main(String[] args) throws Exception
+	{
+		// TODO Auto-generated method stub
+			//Create the object of the class CipherClient
+	    	Client C=new Client();	
+	    	C.goToFirstStep();
+	    
+	}
+
+}
---
+++ SourceCodesProjects/Prasad Patil Projects/File Transfer on UDP sockets in Java with encryption/Server java files/ServerMain.java
@@ -0,0 +1,15 @@
+package serverApplications;
+
+
+
+public class ServerMain {
+
+	public static void main(String[] args) throws Exception 
+	{
+		// TODO Auto-generated method stub
+			//Create an object of class CipherServer
+			Server S = new Server();
+			S.receiveMsg();
+
+	}
+}
